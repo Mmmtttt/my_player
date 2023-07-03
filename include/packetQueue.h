cb4347e8b6d72e1139d8587c0903cf9b92aa3d9c
Mmmtttt <mt1511318385@gmail.com>
2023-07-04 00:20:04 +0800

    1

--- include/packetQueue.h
+++ include/packetQueue.h
@@ -11,26 +11,17 @@
 #include <map>
 class myAVPacket{
     public:
-        myAVPacket(){}
-        myAVPacket(AVPacket pkt):mypkt(pkt),size(pkt.size){num++;}
+        myAVPacket(){is_exist=false;}
+        myAVPacket(AVPacket pkt):mypkt(pkt),size(pkt.size){num++;is_exist=true;}
         ~myAVPacket(){
             //std::cout<<"destory num "<<num<<std::endl;
             av_packet_unref(&mypkt);
         }
-        // int rebuild(){
-        //     size=mypkt.size;
-        //     pts=mypkt.pts;
-        //     duration=mypkt.duration;
-        //     // if((mypkt.flags & AV_PKT_FLAG_KEY)!=0)is_keyframe=true;
-        //     // is_rebuilt=true;
-        // }
+
         AVPacket mypkt;
         int size;
         static int64_t num;//从文件中取出的packet的序号，无论什么类型
-        // bool is_keyframe=false;
-        // bool is_rebuilt=false;
-        // int64_t pts;
-        // int64_t duration;
+        bool is_exist=false;
 };
 
 class packetQueue{
@@ -43,14 +34,13 @@ class packetQueue{
         int packet_queue_pop(std::shared_ptr<myAVPacket>& pkt_ptr, int block);
         void seek(int64_t timeshaft,double timebase){
             std::unique_lock<std::mutex> lock(Mutex);
-            std::cout<<"aaa"<<std::endl;
 
             int64_t dts=pkts_ptr[curr_decode_pos]->mypkt.dts*timebase;
 
             if(timeshaft>dts){
                 while(timeshaft>dts){
                     curr_decode_pos++;
-                    if(curr_decode_pos>=pkts_ptr.size()){curr_decode_pos=pkts_ptr.size();return;}
+                    if(curr_decode_pos>=pkts_ptr.size()){curr_decode_pos=pkts_ptr.size()-1;return;}
                     dts=pkts_ptr[curr_decode_pos]->mypkt.dts*timebase;
                 }
             }
